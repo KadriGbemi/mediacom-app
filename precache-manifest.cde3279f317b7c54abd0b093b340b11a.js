0d379f707fa64a1772240b1f28c0267ff35cee74
kadri <kge.sportcompass.dk>
2019-11-22 20:37:43 +0200

    Updates

---
+++ precache-manifest.cde3279f317b7c54abd0b093b340b11a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddea4d109e7556a1b37538ebea4e8a1",
+    "url": "/mediacom-app/index.html"
+  },
+  {
+    "revision": "9746877f6af6e5f6200d",
+    "url": "/mediacom-app/static/css/2.46b47576.chunk.css"
+  },
+  {
+    "revision": "ba2bfd2f5240b5858212",
+    "url": "/mediacom-app/static/css/main.7d2df2ff.chunk.css"
+  },
+  {
+    "revision": "9746877f6af6e5f6200d",
+    "url": "/mediacom-app/static/js/2.7780fffa.chunk.js"
+  },
+  {
+    "revision": "ba2bfd2f5240b5858212",
+    "url": "/mediacom-app/static/js/main.f95ee5ba.chunk.js"
+  },
+  {
+    "revision": "3178d07b2332f044398a",
+    "url": "/mediacom-app/static/js/runtime-main.823ba2b2.js"
+  },
+  {
+    "revision": "d8e66e15c946d1c340f0c54766bc74cd",
+    "url": "/mediacom-app/static/media/profile_pic.d8e66e15.png"
+  }
+]);
